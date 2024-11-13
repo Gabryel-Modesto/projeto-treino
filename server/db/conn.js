01f3524f5f5a22e095a5ccf3684dc5ab8bcad104
Gabryel Modesto <gabryel.modesto@aluno.senai.br>
2024-11-13 11:59:18 -0300

    query banco

--- server/db/conn.js
+++ server/db/conn.js
@@ -18,3 +18,45 @@ conn.connect((err) => {
 });
 
 module.exports = conn;
+
+/*
+SELECT * FROM users;
+
+CREATE DATABASE teste;
+USE  teste;
+
+CREATE TABLE usuarios(
+	id_usu INT PRIMARY KEY AUTO_INCREMENT,
+    nome_usu VARCHAR(100) NOT NULL,
+    email_usu VARCHAR(100) NOT NULL,
+    password_usu VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE categoria(
+	id_cat INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+    nome_cat VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE produto (
+	id_protudo INT PRIMARY KEY AUTO_INCREMENT,
+    nome_produto VARCHAR(100) NOT NULL,
+    preco_produto DECIMAL(7,2),
+    cat_id INT,
+    FOREIGN KEY (cat_id) REFERENCES categoria(id_cat)
+);
+
+CREATE VIEW cat_produto AS
+SELECT 
+	c.nome_cat,
+	p.nome_produto
+FROM
+produto p
+INNER JOIN 
+categoria c ON p.cat_id = c.id_cat;
+
+SELECT * FROM cat_produto;
+
+SELECT * FROM usuarios;
+
+
+*/
